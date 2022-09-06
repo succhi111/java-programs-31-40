e2421aa20ab7d036bc3bc147532ccb587bdd986c
succhi111 <suchendramishra396@gmail.com>
2022-09-06 14:45:07 +0530

    Program to convert decimal to octal

--- src/DecimalToOctal.java
+++ src/DecimalToOctal.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class DecimalToOctal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any decimal number");
+        int num = sc.nextInt();
+        String b = "";
+        int temp = num;
+
+        while (temp > 0)
+        {
+            int r = temp % 8;
+            temp = temp / 8;
+            b = r + b;
+        }
+        System.out.println("Octal Equivalent of " + num + " is " + b);
+
+    }
 }
+
